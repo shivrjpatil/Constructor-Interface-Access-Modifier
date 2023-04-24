c47c50d703661e44c86353ebd005b62e968c94ac
shivrjpatil <shivrajp972@gmail.com>
2023-04-24 21:32:42 +0530

    Added the types of constructor

---
+++ Constructor-Interface-AccessModifier/Constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Interface_AccessModifier
+{
+    internal class Constructor
+    {
+
+        public Constructor()
+        {
+            Console.WriteLine("This is Parameterless constructor");
+        }
+
+        public  string brand;
+        public int price;
+
+        // parameterized constructor
+        public Constructor(string theBrand, int thePrice)
+        {
+
+            brand = theBrand;
+            price = thePrice;
+        }
+    }
+}
---
+++ Constructor-Interface-AccessModifier/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Interface_AccessModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Default Constructor
+            Constructor constructor2=new Constructor();
+            //calling of Parameterless Constructor
+            new Constructor();
+            //Calling Parameterized constructor
+            Constructor constructor1 = new Constructor("Bugatti", 50000);
+
+            Console.WriteLine("Brand: " + constructor1.brand);
+            Console.WriteLine("Price: " + constructor1.price);
+            Console.ReadLine();
+        }
+    }
+}
