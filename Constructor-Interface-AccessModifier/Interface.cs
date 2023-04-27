a456989ad75eabf1556737c8fe7a36c79fd736b1
shivrjpatil <shivrajp972@gmail.com>
2023-04-27 09:08:06 +0530

    Added Interface Implementation

---
+++ Constructor-Interface-AccessModifier/Interface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Interface_AccessModifier
+{
+    interface Ipig
+    {
+        void PigSound(); // interface method (does not have a body)
+    }
+    interface Idog
+    {
+        void DogSound();
+    }
+
+    // Animal "implements" the Ipig and Idog interfaces
+    class Animal : Ipig, Idog
+    {
+        public void PigSound()
+        {
+            // The body of pigSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+        public void DogSound()
+        {
+            Console.WriteLine("The dog says:Boww Boww");
+        }
+    }
+}
+
--- Constructor-Interface-AccessModifier/Program.cs
+++ Constructor-Interface-AccessModifier/Program.cs
@@ -11,16 +11,34 @@ namespace Constructor_Interface_AccessModifier
     {
         static void Main(string[] args)
         {
-            //Default Constructor
-            Constructor constructor2=new Constructor();
-            //calling of Parameterless Constructor
-            new Constructor();
-            //Calling Parameterized constructor
-            Constructor constructor1 = new Constructor("Bugatti", 50000);
+            Console.WriteLine("1.Types of Constructor\n2.Interface\n3.Access Modifiers");
+            int option = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Brand: " + constructor1.brand);
-            Console.WriteLine("Price: " + constructor1.price);
+            switch(option)
+            {
+                case 1:
+                    //Default Constructor
+                    Constructor constructor2 = new Constructor();
+                    //calling of Parameterless Constructor
+                    new Constructor();
+                    //Calling Parameterized constructor
+                    Constructor constructor1 = new Constructor("Bugatti", 50000);
+
+                    Console.WriteLine("Brand: " + constructor1.brand);
+                    Console.WriteLine("Price: " + constructor1.price);
+                    break;
+                case 2:
+                    Animal animal = new Animal();  // Create a Animal object
+                    animal.PigSound();
+                    animal.DogSound();
+                    break;
+            }
             Console.ReadLine();
+
+
         }
     }
-}
+} 
+        
+    
+
